2d624066faa9d525819f29e19d9e371433ab6811
massi-css <sebiha_massinissa@etu.univ-blida.dz>
2023-10-13 15:15:40 +0100

    perfection

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-gsap.fromTo('.loading-page', {opacity: 1}, {opacity: 0, duration: 1.5, delay: 3.5, display: 'none'})
+gsap.fromTo('.loading-page', {opacity: 1}, {height:0 , duration: 1, delay: 3.5, ease: 'expo.inOut'})
 gsap.fromTo('.logo-name',{y:50, opacity: 0},{y:0, opacity: 1, duration: 2 ,delay: 1 } )
